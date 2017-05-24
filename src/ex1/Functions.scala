0332d2d955f8c0f09073e1007a6b722a76d4a1c3
DimitarBakardzhiev <dimitar.bakardzhiev94@gmail.com>
2017-05-24 16:46:34 +0300

    Added homework 1 and 2.

--- src/ex1/Functions.scala
+++ src/ex1/Functions.scala
@@ -3,33 +3,83 @@ package ex1
 object Functions {
 
   // Връща големината на масив (без да ползва data.length!!!)
-  def length(data: List[Int]) = ??? 
+  def length(data: List[Int]) = {
+    def _length(data: List[Int], count: Int): Int = {
+      if (data.isEmpty) count
+      else _length(data.tail, count + 1)
+    }
+
+    _length(data, 0)
+  }
 
   // Ако cond е true връща onTrue
-  def ifelse(cond: Boolean, onTrue: Int, onFalse: Int) = ???
+  def ifelse(cond: Boolean, onTrue: => Int, onFalse: => Int) = {
+    if (cond) onTrue
+    else onFalse
+  }
 
   // Проверява дали скобите в даден масив от символи са балансирани.
   // Коректно: (a)asda(b)(v) | (((a))) | ()(()асдасд)
-  // Грешно: )() | ((д) | ((das) (d)( 
-  def balance(chars: List[Char]): Boolean = ???
+  // Грешно: )() | ((д) | ((das) (d)(
+  def balance(chars: List[Char]): Boolean = {
+    def _balance(chars: List[Char], openBrackets: Int): Boolean = {
+      if (openBrackets < 0) false
+      if (chars.isEmpty) openBrackets == 0
+      else {
+        if (chars.head == '(') _balance(chars.tail, openBrackets + 1)
+        else if (chars.head == ')') _balance(chars.tail, openBrackets - 1)
+        else _balance(chars.tail, openBrackets)
+      }
+    }
+
+    _balance(chars, 0)
+  }
 
   def map(chars: List[Char], f: Any) =  ???
 
-  def toUpperCase(chars: List[Char]) = {
-    def upperCase(char: Char) = ???
-  
-    ???
+  def toUpperCase(chars: List[Char]): List[Char] = {
+    def upperCase(char: Char) = {
+      val ascii = char - 32
+      ascii.toChar
+    }
+
+    if (chars.isEmpty) chars
+    else {
+      upperCase(chars.head) :: toUpperCase(chars.tail)
+    }
   }
 
   // Проверява дали съществува елемент отговарящ на f
-  def exists(data: List[Int], f: Any) = ???
+  def exist(data: List[Int], f: (Int) => Boolean): Boolean = {
+    if (data.isEmpty) false
+    else
+      if (f(data.head)) true
+      else exist(data.tail, f)
+  }
 
   // Връща масив съдържащ само елементите отговарящи на f
-  def filter(data: List[Int], f: Any) = ???
+  def filter(data: List[Int], f: (Int) => Boolean): List[Int]  = {
+    if (data.isEmpty) data
+    else
+      if (f(data.head)) data.head :: filter(data.tail, f)
+      else filter(data.tail, f)
+  }
 
   // Проверява дали всички елементи отговарят на f
-  def forall(data: List[Int], f: Any) = ???
+  def forall(data: List[Int], f: (Int) => Boolean): Boolean = {
+    if (data.isEmpty) true
+    else
+      if (!f(data.head)) false
+      else exist(data.tail, f)
+  }
 
   // Връща числото от триъгълника на Паскал отговарящо на съответния ред/колона
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    def factorial(n: Int): Int = {
+      if (n == 0 || n == 1) 1
+      else n * factorial(n - 1)
+    }
+
+    factorial(r) / (factorial(c) * factorial(r - c))
+  }
 }
--- src/ex2/Architect.scala
+++ src/ex2/Architect.scala
@@ -16,10 +16,42 @@ class Architect {
    *     val o: Option[Int] = Some(6)
    *     val n: Int = o.get
    */
-  def max(xs: List[Int]): Option[Int] = ???
+  def max(xs: List[Int]): Option[Int] = {
+    def _max(xs: List[Int], max: Int): Option[Int] = {
+      if (xs.isEmpty) {
+        Some(max)
+      } else {
+        if (xs.head > max) {
+          _max(xs.tail, xs.head)
+        } else {
+          _max(xs.tail, max)
+        }
+      }
+    }
+
+    if (xs.isEmpty) {
+      None
+    } else {
+      _max(xs, 2000000000)
+    }
+  }
 
   // Determines the type of given triangle: "rectangular", "equilateral", "isosceles", "random"
-  def triangleType(t: Triangle): String = ???
+  def triangleType(t: Triangle): String = {
+    if (t.a == t.b && t.b == t.c) {
+      "equilateral"
+    }
+
+    if (t.a == t.b || t.b == t.c || t.c == t.a) {
+      if (Math.pow(t.a, 2) + Math.pow(t.b, 2) == Math.pow(t.c, 2)) {
+        "isosceles rectangular"
+      }
+
+      "isosceles"
+    }
+
+    "random"
+  }
 
   /*
    * Calculates the area of the provided shape, by using these formulae:
@@ -28,18 +60,50 @@ class Architect {
    *  - Rectangle: a * b, where a and b are both sides
    *  - Trapezoid: (a + b) * h / 2, where a and b are the parallel sides and h is the height between them
    *  - Cube: always return -1
-   *  
+   *
    *  Hint: for triangles use the max function
    */
-  def area(s: Shape): Double = ???
+  def area(s: Shape): Double = s match {
+    case t: Triangle => triangleType(t) match {
+      case "rectangular" => max(List(t.a, t.b, t.c)).get match {
+        case t.a => (t.b * t.c) / 2
+        case t.b => (t.a * t.c) / 2
+        case t.c => (t.a * t.b) / 2
+      }
+      case _ => (max(List(t.a, t.b, t.c)).get * t.h) / 2
+    }
+    case r: Rectangle => r.a * r.b
+    case tr: Trapezoid => (tr.a + tr.b) * (tr.h / 2)
+    case c: Cube => -1
+  }
 
   /*
    *  Returns the number of rectangular triangles in given list of shapes
-   *  
+   *
    *  Hint: use the triangleType function
    */
   def findRectangulars(shapes: List[Shape]): Int = {
-    def iter(shapes: List[Shape], n: Int): Int = ???
-    iter(???, ???)
+    def iter(shapes: List[Shape], n: Int): Int = {
+      if (shapes.isEmpty) {
+        n
+      } else {
+        shapes.head match {
+          case s: Triangle => {
+            triangleType(s) match {
+              case "rectangular" => iter(shapes.tail, n + 1)
+              case _ => iter(shapes.tail, n)
+            }
+          }
+
+          case _ => iter(shapes.tail, n)
+        }
+      }
+    }
+
+    if (shapes.isEmpty) {
+      0
+    } else {
+      iter(shapes, 0)
+    }
   }
 }
